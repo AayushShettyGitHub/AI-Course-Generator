b584a796eb39b7c9fbe639c5be599e71d85345ce
Aayush Shetty <aayushshetty03@gmail.com>
2026-03-23 18:38:21 +0530

    Made frontend deployment changes

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -2,4 +2,8 @@ const config = {
     API_BASE_URL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8082",
 };
 
+if (import.meta.env.PROD && !import.meta.env.VITE_API_BASE_URL) {
+    config.API_BASE_URL = "https://aicg-fawn.vercel.app";
+}
+
 export default config;
